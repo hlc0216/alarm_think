1247e9a86f3b23a1b16d2e7899d3f70023177291
hlc0216 <1582450708@qq.com>
2020-09-17 09:39:58 +0800

    实现36个excel文件的单一告警统计

--- venv/Include/onenet_warning1/get_onenet_unique_warning.py
+++ venv/Include/onenet_warning1/get_onenet_unique_warning.py
@@ -20,7 +20,15 @@ def test_files(dir_path):
     # file_path = os.path.join(dir_path, files_name)
     print('在设备-厂家-网元表的基础上，求出单一网元的唯一告警.........')
     print(file_path)
-def get_onenet_unique_warning (file_path,to_dir_path):
+def get_onenet_unique_warning(dir_path,to_dir_path):
+    files_name=os.listdir(dir_path)#得到所有文件的文件名
+    for file_name in files_name:
+        file_path=os.path.join(dir_path,file_name)
+        print(file_path)
+        get_onenet_unique_warning0(file_path,file_name,to_dir_path)
+    print('全部单一告警统计完毕！')
+
+def get_onenet_unique_warning0 (file_path,file_name,to_dir_path):
     # files_name=os.listdir(dir_path)#得到所有文件的文件名
     # file_path=os.path.join(dir_path,files_name)
     print('在设备-厂家-网元表的基础上，求出单一网元的唯一告警.........')
@@ -30,8 +38,11 @@ def get_onenet_unique_warning (file_path,to_dir_path):
     df = pd.read_excel(file_path, sheet_name=None)
     sheets_name = list(df)
     # 获得表头，为后边添加表头使用
-    df_head = pd.read_excel(file_path, sheet_name=1)
-    df_head_list = [df_head.columns.tolist()]
+    # df_head = pd.read_excel(file_path, sheet_name=1)
+
+    df_head_list = [['告警来源', '对象名称', '（告警）地市名称', '区县', '机房名称', '网元名称', '设备类型', '设备厂家', '网管告警级别', '告警标题', '厂家原始告警级别', '告警发生时间', '告警清除时间', '工单号', '派单所使用的规则', '派单状态', '未派单原因', '派单失败原因', '工单状态', '告警备注', '告警指纹fp0', '告警指纹fp1', '告警指纹fp2', '告警指纹fp3', '告警清除状态', '清除告警指纹fp0', '清除告警指纹fp1', '清除告警指纹fp2', '清除告警指纹fp3']]
+    # print(df_head_list)
+    #告警来源	对象名称	（告警）地市名称	区县		网元名称		设备厂家	网管告警级别	告警标题	厂家原始告警级别	告警发生时间	告警清除时间	工单号	派单所使用的规则	派单状态	未派单原因	派单失败原因	工单状态	告警备注	告警指纹fp0	告警指纹fp1	告警指纹fp2	告警指纹fp3	告警清除状态	清除告警指纹fp0	清除告警指纹fp1	清除告警指纹fp2	清除告警指纹fp3
     remeber_nrows=[]#记录保存的行
     nrows_values = []
     for i in range (1,len(sheets_name)):
@@ -50,7 +61,9 @@ def get_onenet_unique_warning (file_path,to_dir_path):
                 df_others_net = pd.read_excel(file_path,sheet_name=j)
                 # print('获取others_net_list')
                 others_list += df_others_net['告警标题'].values.tolist()
-
+        # if len(others_list)==0:
+        #
+        #     break
         #对比onenet_list和others_list，求取行数
         nrows=[]
         for j in range (len(onenet_list)):
@@ -74,20 +87,10 @@ def get_onenet_unique_warning (file_path,to_dir_path):
     nrows_values= df_head_list+nrows_values
 
     df_write_net = pd.DataFrame(nrows_values)
-    to_file_name='中兴设备scscf单一告警.xlsx'
+
+    to_file_name = '单一告警'+file_name
     to_file_path = os.path.join(to_dir_path,to_file_name)
     df_write_net.to_excel(to_file_path,index=None,header=None)
-    # to_file_path = file_path
-    # sheet_name='get_onenet_unique_warning'
-    # excelWriter = pd.ExcelWriter(to_file_path, engine='openpyxl')
-    # hcu._excelAddSheet(df_write_net, excelWriter, sheet_name)  # 为excel添加新的sheet，并将dataframe写入新的sheet（不覆盖其他sheet）
-    # # print('%s网元处理数据完成，写入文件成功！' % file_name)
-
-
-
-
-
-
 
     print('remeber_nrows=', remeber_nrows)
     # print(nrows_values)
@@ -98,21 +101,14 @@ def get_onenet_unique_warning (file_path,to_dir_path):
 
 
 
-
-
-
-
-
 if __name__ == '__main__':
     #主函数测试用
     abspath = os.path.abspath('../../../data')  # 设置相对路径（基准路径）
     # 1、处理源数据得出各个设备表（sbc、scscf、hss fe等）
     dir_path = abspath + r'\onenet_warning_data1\get_equipment_factory_netcell'
     to_dir_path = abspath + r'\onenet_warning_data1\get_onenet_unique_warning'
-    file_name = r'中兴设备scscf.xlsx'
-    file_path = os.path.join(dir_path, file_name)
-    # equip_col_name = '设备类型'
-    get_onenet_unique_warning(file_path,to_dir_path)
 
+
+    get_onenet_unique_warning(dir_path,to_dir_path)
 
 
